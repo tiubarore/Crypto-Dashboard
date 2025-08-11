b580750089605481368177ecc9077e1464e58f82
Vincenzo Nicassio <tiubarore93@gmail.com>
2025-08-11 16:10:22 +0200

    added search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import CoinCard from "./components/CoinCard";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 const App = () => {
   const [coins, setCoins] = useState([]);
   const [isLoading, setisLoading] = useState(true);
@@ -10,7 +12,7 @@ const App = () => {
     const fetchCoins = async () => {
       try {
         const res = await fetch(
-          "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market-cap-desc&per_page=10&page=1&sparkline=false"
+          `${API_URL}&order=market-cap-desc&per_page=10&page=1&sparkline=false`
         );
         if (!res.ok) throw new Error("Failed to fetch data");
         const data = await res.json();
